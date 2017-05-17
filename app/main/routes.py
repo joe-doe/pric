6f9fbd1bbaa0289e0808f00f09d449b5de92f288
joe-doe <joe.doe@engineer.com>
2017-05-17 16:47:22 +0300

    get rid of flask-wtf

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,20 +1,16 @@
 from flask import session, redirect, url_for, render_template, request
 from . import main
-from .forms import LoginForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     """"Login form to enter a room."""
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['name'] = form.name.data
-        session['room'] = form.room.data
+    if request.method == 'POST':
+        session['name'] = request.form.get('name')
+        session['room'] = request.form.get('room')
         return redirect(url_for('.chat'))
     elif request.method == 'GET':
-        form.name.data = session.get('name', '')
-        form.room.data = session.get('room', '')
-    return render_template('index.html', form=form)
+        return render_template('index.html')
 
 
 @main.route('/chat')
