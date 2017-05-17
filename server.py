6e81bf1718098db3c89cf13752169bff3358b4ce
joe-doe <joe.doe@engineer.com>
2017-05-17 15:10:43 +0300

    append messages

--- server.py
+++ server.py
@@ -5,14 +5,17 @@ import json
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
 @app.route("/")
 def index():
-    return render_template('chat.html',)
+    return render_template('chat.html', )
+
 
 @socketio.on('send_message')
 def handle_source(json_data):
     text = json_data['message'].encode('ascii', 'ignore')
-    socketio.emit('echo', {'echo': 'Server Says: '+text})
+    socketio.emit('echo', {'echo': 'Server Says: ' + text})
+
 
 if __name__ == "__main__":
     socketio.run(app)
