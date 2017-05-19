3cb21c9d154add43d4c97e0cbf58b44e801ca2fd
joe-doe <joe.doe@engineer.com>
2017-05-19 16:00:04 +0300

    emoji

--- app/main/events.py
+++ app/main/events.py
@@ -17,7 +17,7 @@ def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+    emit('message', {'user': session.get('name'), 'msg': message['msg']}, room=room)
 
 
 @socketio.on('left', namespace='/chat')
--- app/static/js/websocket_client.js
+++ app/static/js/websocket_client.js
@@ -1,9 +1,10 @@
             var socket;
+
             $(document).ready(function(){
 
-                var prot = "https";
+                var prot = "http";
                 var app_url = prot + '://' + document.domain + ':' + location.port;
-
+                var static_folder = $('.static-folder').attr('id');
 
                 socket = io.connect(app_url + '/chat');
                 socket.on('connect', function() {
@@ -14,7 +15,14 @@
                     $('#chat').scrollTop($('#chat')[0].scrollHeight);
                 });
                 socket.on('message', function(data) {
-                    $('#chat').val($('#chat').val() + data.msg + '\n');
+
+                    // msg = data.msg.replace(":)", "<img src='" + static_folder + "photo/emoji/rockanim.gif' alt='rock-emoji'/>");
+                    for(key in emoji) {
+                        msg = data.msg.replace(key.toString(), "<img src='" + static_folder + "photo/emoji/"+ emoji[key] +"' alt='"+ emoji[key] +"'/>");
+                        console.log(msg);
+                    }
+
+                    $('#chat').append('<span id="user">' + data.user + ':</span><span id="message">' + msg + '</span><br />');
                     $('#chat').scrollTop($('#chat')[0].scrollHeight);
                     if (focused == false) {
                         notifyMe(data.msg);
@@ -28,7 +36,18 @@
                         socket.emit('text', {msg: text});
                     }
                 });
+
+//                function parse_for_emoji(msg) {
+//                    var modified_message;
+//
+//                    for(key in emoji) {
+//                        modified_message = msg.replace(key, "<img src='" + static_folder + "photo/emoji/"+ emoji[key] +"' alt='"+ emoji[key] +"'/>");
+//                    }
+//                    return modified_message;
+//                }
             });
+
+
 
             function leave_room() {
                 socket.emit('left', {}, function() {
