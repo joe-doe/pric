4eaa51e8f35374246970cbd74dfeccbba27ac7f9
Evangelos Rokkos <e.rokkos@encodegroup.com>
2017-06-01 11:22:58 +0300

    fix leave room redirect url

--- app/static/js/websocket_client.js
+++ app/static/js/websocket_client.js
@@ -1,59 +1,48 @@
-            var socket;
+var socket;
+var prot = "http";
+var app_url = prot + '://' + document.domain + ':' + location.port;
 
-            $(document).ready(function(){
+$(document).ready(function(){
 
-                var prot = "http";
-                var app_url = prot + '://' + document.domain + ':' + location.port;
-                var static_folder = $('.static-folder').attr('id');
+    var static_folder = $('.static-folder').attr('id');
+    socket = io.connect(app_url + '/chat');
 
-                socket = io.connect(app_url + '/chat');
-                socket.on('connect', function() {
-                    socket.emit('joined', {});
-                });
-                socket.on('status', function(data) {
-                    $('#chat').val($('#chat').val() + '< ' + data.msg + ' >\n');
-                    $('#chat').scrollTop($('#chat')[0].scrollHeight);
-                });
-                socket.on('message', function(data) {
+    socket.on('connect', function() {
+        socket.emit('joined', {});
+    });
 
-                    // msg = data.msg.replace(":)", "<img src='" + static_folder + "photo/emoji/rockanim.gif' alt='rock-emoji'/>");
-                    for(key in emoji) {
-                        msg = data.msg.replace(key.toString(), "<img src='" + static_folder + "photo/emoji/"+ emoji[key] +"' alt='"+ emoji[key] +"'/>");
-                        console.log(msg);
-                    }
+    socket.on('status', function(data) {
+        $('#chat').val($('#chat').val() + '< ' + data.msg + ' >\n');
+        $('#chat').scrollTop($('#chat')[0].scrollHeight);
+    });
 
-                    $('#chat').append('<span id="user">' + data.user + ':</span><span id="message">' + msg + '</span><br />');
-                    $('#chat').scrollTop($('#chat')[0].scrollHeight);
-                    if (focused == false) {
-                        notifyMe(data.msg);
-                    }
-                });
-                $('#text').keypress(function(e) {
-                    var code = e.keyCode || e.which;
-                    if (code == 13) {
-                        text = $('#text').val();
-                        $('#text').val('');
-                        socket.emit('text', {msg: text});
-                    }
-                });
+    socket.on('message', function(data) {
+        // msg = data.msg.replace(":)", "<img src='" + static_folder + "photo/emoji/rockanim.gif' alt='rock-emoji'/>");
+        for(key in emoji) {
+            msg = data.msg.replace(key.toString(), "<img src='" + static_folder + "photo/emoji/"+ emoji[key] +"' alt='"+ emoji[key] +"'/>");
+            console.log(msg);
+        }
+        $('#chat').append('<span id="user">' + data.user + ': </span><span id="message"> ' + msg + '</span><br />');
+        $('#chat').scrollTop($('#chat')[0].scrollHeight);
+        if (focused == false) {
+            notifyMe(data.msg);
+        }
+    });
 
-//                function parse_for_emoji(msg) {
-//                    var modified_message;
-//
-//                    for(key in emoji) {
-//                        modified_message = msg.replace(key, "<img src='" + static_folder + "photo/emoji/"+ emoji[key] +"' alt='"+ emoji[key] +"'/>");
-//                    }
-//                    return modified_message;
-//                }
-            });
+    $('#text').keypress(function(e) {
+        var code = e.keyCode || e.which;
+        if (code == 13) {
+            text = $('#text').val();
+            $('#text').val('');
+            socket.emit('text', {msg: text});
+        }
+    });
+});
+function leave_room() {
+        socket.emit('left', {}, function() {
+            socket.disconnect();
+            // go back to the login page
+            window.location.href = app_url;
+        });
+}
 
-
-
-            function leave_room() {
-                socket.emit('left', {}, function() {
-                    socket.disconnect();
-
-                    // go back to the login page
-                    window.location.href = "{{ url_for('main.index') }}";
-                });
-            }
